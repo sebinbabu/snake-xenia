48f43aa126783d4f2ed59798e9a951eeb39e7190
Sebin Babu <sebin1998@gmail.com>
2018-08-08 13:18:26 +0530

    initialized save commit

---
+++ save.c
@@ -0,0 +1,22 @@
+#include "save.h"
+#include "structs.h"
+
+void initSave(game *g) {
+	metasave m = {g->score, g->speed, g->quantum};
+	int s = 0, max = 100;
+	node *t;	
+	FILE *out;
+	out = fopen("SAVEFILE", "w");
+	if(out == NULL) {
+		printw("ERROR WRITING FILE");
+		return;
+	}
+	savenode sav[100];
+
+	fwrite(&m, sizeof(metasave), 1, out);
+	t = g->s->t;
+	while(t != NULL) {
+		
+	}
+}
+
